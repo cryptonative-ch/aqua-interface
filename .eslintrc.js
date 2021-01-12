686afac34071a815be51ffc17dc00bc90080aa94
Adam Azad <adam@adamazad.com>
2021-01-12 20:00:24 +0300

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': 0,
+  },
+}
