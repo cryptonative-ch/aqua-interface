841bea5b3c9c49baf0eec0d670685356eb6639d9
Adam Azad <adam@adamazad.com>
2021-01-19 19:35:51 +0300

    Add Link Component

---
+++ src/components/Link.tsx
@@ -0,0 +1,33 @@
+import { size, SizeProps, space, variant } from 'styled-system'
+import { Link as LinkBase } from 'react-router-dom'
+import styled from 'styled-components'
+import { theme } from 'src/styles/theme'
+
+type LinkProps = SizeProps & {
+  variant?: string
+}
+
+export const Link = styled(LinkBase)<LinkProps>(
+  props => ({
+    fontWeight: 'bold',
+  }),
+  size,
+  space,
+  variant({
+    variants: {
+      default: {
+        color: theme.colors.secondary,
+        ':hover': {
+          color: theme.colors.secondary,
+        },
+      },
+      button: {
+        padding: `${theme.space[2]}px ${theme.space[2]}px`,
+      },
+    },
+  })
+)
+
+Link.defaultProps = {
+  variant: undefined,
+}
