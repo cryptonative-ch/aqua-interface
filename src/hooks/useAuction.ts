cf791968beb0158364b31e0a718e5a9dadc3961a
Adam Azad <adam@adamazad.com>
2021-01-19 19:36:09 +0300

    Add Hooks

---
+++ src/hooks/useAuction.ts
@@ -0,0 +1,23 @@
+// External
+import { useEffect, useState } from 'react'
+
+// Hooks
+import { useAuctions } from './useAuctions'
+
+// Interfaces
+import { Auction } from 'src/interfaces/Auction'
+
+/**
+ *
+ * @param auctionId
+ */
+export function useAuction(auctionId: string) {
+  const auctions = useAuctions()
+  const [auction, setAuctions] = useState<Auction>()
+
+  useEffect(() => {
+    setAuctions(auctions.find(auction => auction.id === auctionId))
+  }, [auctions])
+
+  return auction
+}
