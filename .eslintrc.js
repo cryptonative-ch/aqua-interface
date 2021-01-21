fd22065f5d68ca724d502da93fd1ae6945fc9dc9
Adam Azad <adam@adamazad.com>
2021-01-21 22:02:49 +0300

    Extend ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,23 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
     sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   plugins: ['react'],
   rules: {
-    'linebreak-style': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
   },
 }
