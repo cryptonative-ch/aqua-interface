72dbf8320d7216a52ca073cdb1ef109fbc796934
Adam Azad <adam@adamazad.com>
2021-01-21 18:55:49 +0300

    Add more hooks

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -10,3 +10,9 @@ import { INFURA_API_KEY } from 'src/constants'
 export const getInfuraProvider = () => {
   return new InfuraProvider('homestead', INFURA_API_KEY)
 }
+
+let BLOCK_NUMBER = 11699666
+
+setInterval(() => BLOCK_NUMBER++, 15000)
+
+export const getBlockNumber = async () => BLOCK_NUMBER
