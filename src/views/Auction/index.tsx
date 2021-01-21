8d4743a7caeda900d4285c2f19f0a35dacad7850
Adam Azad <adam@adamazad.com>
2021-01-21 18:56:00 +0300

    Update Auction view

--- src/views/Auction/index.tsx
+++ src/views/Auction/index.tsx
@@ -1,29 +1,37 @@
 // External
+import styled, { useTheme } from 'styled-components'
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import React, { useEffect } from 'react'
 
 // Hooks
-import { useMountEffect } from 'src/hooks/useMountEffect'
 import { useAuction } from 'src/hooks/useAuction'
 
 // Actions
 import { setPageTitle } from 'src/redux/page'
 
 // Components
+import { PlaceBidForm } from './components/PlaceBidForm'
 import { Container } from 'src/components/Container'
+import { CardTitle } from 'src/components/CardTitle'
 import { CardBody } from 'src/components/CardBody'
+import { BidList } from './components/BidList'
 import { Header } from './components/Header'
 import { Card } from 'src/components/Card'
+import { Flex } from 'src/components/Flex'
 
 // Layouts
 import { Center } from 'src/layouts/Center'
 
 // Views
 import { NotFoundView } from '../NotFound'
-import { CardTitle } from 'src/components/CardTitle'
-import { useTheme } from 'styled-components'
+
+// Interfaces
+import { AuctionBid } from 'src/interfaces/Auction'
+
+// Mesa Utils
+import { calculateClearingPrice } from 'src/mesa/utils'
 
 interface AuctionViewParams {
   auctionId: string
@@ -44,21 +52,51 @@ export function AuctionView() {
     return <NotFoundView />
   }
 
+  if (!auction) {
+    return <NotFoundView />
+  }
+
   return (
     <Center minHeight="100%">
       <Container>
-        <Header title={t('auction.tokenName')} />
+        <Header title="Simulation" />
+        <Card mb={theme.space[4]}>
+          <CardBody>
+            <CardTitle>{t('texts.bids')}</CardTitle>
+          </CardBody>
+          <CardBody>{JSON.stringify(calculateClearingPrice(auction), null, 2)}</CardBody>
+        </Card>
+        <FlexGroupColumns>
+          <Card mb={theme.space[4]}>
+            <CardBody>
+              <CardTitle>{t('texts.placeBid')}</CardTitle>
+            </CardBody>
+            <CardBody>
+              <PlaceBidForm onSubmit={({ tokenAmount, tokenPrice }) => {}} />
+            </CardBody>
+          </Card>
+          <Card mb={theme.space[4]}>
+            <CardBody>
+              <CardTitle>{t('texts.bids')}</CardTitle>
+            </CardBody>
+            <CardBody>
+              <BidList baseTokenSymbol="DAI" quotetokenSmybol={auction.tokenSymbol} bids={auction.bids} />
+            </CardBody>
+          </Card>
+        </FlexGroupColumns>
+        <Card mb={theme.space[4]}>
+          <CardBody>
+            <CardTitle>{t('texts.yourBids')}</CardTitle>
+          </CardBody>
+        </Card>
       </Container>
-      <Card mb={theme.space[3]}>
-        <CardBody>
-          <CardTitle>{t('texts.graph')}</CardTitle>
-        </CardBody>
-      </Card>
-      <Card mb={theme.space[3]}>
-        <CardBody>
-          <CardTitle>{t('texts.bids')}</CardTitle>
-        </CardBody>
-      </Card>
     </Center>
   )
 }
+
+const FlexGroupColumns = styled(Flex)(props => ({
+  gap: props.theme.space[4],
+  '& > *': {
+    flex: 1,
+  },
+}))
