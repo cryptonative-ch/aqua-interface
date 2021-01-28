ca76bf6ed95c1037f3af8cba04885300367fff69
Adam Azad <adam@adamazad.com>
2021-01-28 18:01:01 +0300

    Remove bignumber.js

--- src/hooks/useTokenBalance.ts
+++ src/hooks/useTokenBalance.ts
@@ -1,17 +1,18 @@
+// External
 import { useWeb3React } from '@web3-react/core'
 import { useEffect, useState } from 'react'
-import { BigNumber } from 'bignumber.js'
+import { BigNumber } from 'ethers'
 
 /**
- * Return the balance owned by the address
+ * Returns the balance owned by the address
  */
-export function useTokenBalance(address: string) {
-  const [balance, setBalance] = useState<BigNumber>(new BigNumber(0))
-  const { library } = useWeb3React()
+export function useTokenBalance(tokenContractAddress: string) {
+  const [balance, setBalance] = useState<BigNumber>(BigNumber.from(0))
+  const { library, account } = useWeb3React()
 
   useEffect(() => {
-    setBalance(new BigNumber(6535))
-  }, [library])
+    setBalance(BigNumber.from(6535))
+  }, [library, account])
 
   return balance
 }
