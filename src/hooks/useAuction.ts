ec69e3013adc394da281f694c3e30986917abade
Adam Azad <adam@adamazad.com>
2021-02-08 18:15:57 +0300

    Restructure useAuction(s) hooks

--- src/hooks/useAuction.ts
+++ src/hooks/useAuction.ts
@@ -7,17 +7,39 @@ import { useAuctions } from './useAuctions'
 // Interfaces
 import { Auction } from 'src/interfaces/Auction'
 
+interface UseAuctionReturn {
+  loading: boolean
+  error: Error | null
+  auction: Auction | null
+}
+
 /**
  *
  * @param auctionId
  */
-export function useAuction(auctionId: string) {
-  const auctions = useAuctions()
-  const [auction, setAuctions] = useState<Auction>()
+export function useAuction(auctionId: string): UseAuctionReturn {
+  const { auctions } = useAuctions()
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<Error | null>(null)
+  const [auction, setAuctions] = useState<Auction | null>(null)
 
   useEffect(() => {
-    setAuctions(auctions.find(auction => auction.id === auctionId))
+    try {
+      const foundAuction = auctions.find(auction => auction.id === auctionId)
+
+      // Auction == null = not found
+      if (foundAuction) {
+        setAuctions(foundAuction)
+      }
+    } catch (e) {
+      setError(e)
+    }
+    setLoading(false)
   }, [auctions, auctionId])
 
-  return auction
+  return {
+    auction,
+    loading,
+    error,
+  }
 }
