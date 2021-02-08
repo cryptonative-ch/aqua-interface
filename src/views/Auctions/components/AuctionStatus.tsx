d60cde705d262860ef4c71002507b8afa11ca29d
user <hamza.h.hamud@gmail.com>
2021-02-08 10:38:28 +0000

    converted auction status div to local time from UTC

--- src/views/Auctions/components/AuctionStatus.tsx
+++ src/views/Auctions/components/AuctionStatus.tsx
@@ -7,18 +7,21 @@ import { isAuctionClosed, isAuctionOpen, isAuctionUpcoming } from 'src/mesa/auct
 // Interfaces
 import { Auction } from 'src/interfaces/Auction'
 
+// Utils
+import { convertUtcTimestampToLocal } from 'src/utils/date'
+
 interface AuctionStatusComponentProps {
   auction: Auction
 }
 
 export function AuctionStatus({ auction }: AuctionStatusComponentProps) {
   if (isAuctionOpen(auction)) {
-    return <div>Ends {dayjs().to(dayjs.unix(auction.endBlock))}</div>
+    return <div>Ends {dayjs().to(dayjs.unix(convertUtcTimestampToLocal(auction.endBlock)))}</div>
   } else if (isAuctionUpcoming(auction)) {
     console.log(auction)
-    return <div>Starts {dayjs().to(dayjs.unix(auction.startBlock))}</div>
+    return <div>Starts {dayjs().to(dayjs.unix(convertUtcTimestampToLocal(auction.startBlock)))}</div>
   } else if (isAuctionClosed(auction)) {
-    return <div>Ended {dayjs().to(dayjs.unix(auction.endBlock))}</div>
+    return <div>Ended {dayjs().to(dayjs.unix(convertUtcTimestampToLocal(auction.endBlock)))}</div>
   }
 
   return <div>{dayjs().to(dayjs())}</div>
