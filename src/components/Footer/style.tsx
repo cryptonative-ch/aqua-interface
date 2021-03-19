74b275cfc74d29620280776bc2ae0052de4d8db2
Light Fury <leech.developer@gmail.com>
2021-03-19 04:16:24 -0600

    update coding style

--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,32 +1,32 @@
 //External
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
-  height: 72px;
-  width: 100%;
-  padding: 0 20px;
-  outline: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`
+export const Wrapper = styled.div({
+  height: '72px',
+  width: '100%',
+  padding: '0 20px',
+  outline: '0',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
 
-export const Row = styled.div`
-  padding: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-`
+export const Row = styled.div({
+  padding: '0',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+})
 
-export const Title = styled.div`
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 21px;
-  color: #7b7f93;
-  padding: 0 12px;
-  cursor: pointer;
-  user-select: none;
-`
+export const Title = styled.div({
+  fontStyle: 'normal',
+  fontWeight: 500,
+  fontSize: '14px',
+  lineHeight: '21px',
+  color: '#7b7f93',
+  padding: '0 12px',
+  cursor: 'pointer',
+  userSelect: 'none',
+})
