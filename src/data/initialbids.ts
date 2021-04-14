e3bcf3c15f9f6b73cb6e0def7861af7f931e9b46
Martin Krung <mk@cryptonative.ch>
2021-04-14 21:55:49 +0200

    flip for having the right bids

--- src/data/initialbids.ts
+++ src/data/initialbids.ts
@@ -1,112 +1,112 @@
 export const initialBid = [
   {
     address: '0xf1',
-    tokenOut: 1000, // price: 1.3
-    tokenIn: 769.230769230769,
+    tokenIn: 1000, // price: 1.3
+    tokenOut: 769.230769230769,
   },
   {
     address: '0xf2',
-    tokenOut: 15000, // price: 1.2
-    tokenIn: 12500,
+    tokenIn: 15000, // price: 1.2
+    tokenOut: 12500,
   },
   {
     address: '0xf3',
-    tokenOut: 1000, // price: 1.2
-    tokenIn: 833.333333333333,
+    tokenIn: 1000, // price: 1.2
+    tokenOut: 833.333333333333,
   },
   {
     address: '0xf4',
-    tokenOut: 1000, // price: 1.15
-    tokenIn: 869.565217391304,
+    tokenIn: 1000, // price: 1.15
+    tokenOut: 869.565217391304,
   },
   {
     address: '0xf5',
-    tokenOut: 1000, // price: 1.1
-    tokenIn: 909.090909090909,
+    tokenIn: 1000, // price: 1.1
+    tokenOut: 909.090909090909,
   },
   {
     address: '0xf6',
-    tokenOut: 10000, // price: 1.05
-    tokenIn: 9523.80952380952,
+    tokenIn: 10000, // price: 1.05
+    tokenOut: 9523.80952380952,
   },
   {
     address: '0xf7',
-    tokenOut: 1000, // price: 1.05
-    tokenIn: 952.380952380952,
+    tokenIn: 1000, // price: 1.05
+    tokenOut: 952.380952380952,
   },
   {
     address: '0xf8',
-    tokenOut: 5000, // price: 0.95
-    tokenIn: 5263.15789473684,
+    tokenIn: 5000, // price: 0.95
+    tokenOut: 5263.15789473684,
   },
   {
     address: '0xf9',
-    tokenOut: 500, // price: 0.91
-    tokenIn: 549.450549450549,
+    tokenIn: 500, // price: 0.91
+    tokenOut: 549.450549450549,
   },
   {
     address: '0xf10',
-    tokenOut: 10000, // price: 0.85
-    tokenIn: 11764.7058823529,
+    tokenIn: 10000, // price: 0.85
+    tokenOut: 11764.7058823529,
   },
   {
     address: '0xf11',
-    tokenOut: 2000, // price: 0.77
-    tokenIn: 2597.4025974026,
+    tokenIn: 2000, // price: 0.77
+    tokenOut: 2597.4025974026,
   },
   {
     address: '0xf12',
-    tokenOut: 2000, // price: 0.765
-    tokenIn: 2614.37908496732,
+    tokenIn: 2000, // price: 0.765
+    tokenOut: 2614.37908496732,
   },
   {
     address: '0xf13',
-    tokenOut: 2000, // price: 0.76
-    tokenIn: 2631.57894736842,
+    tokenIn: 2000, // price: 0.76
+    tokenOut: 2631.57894736842,
   },
   {
     address: '0xf14',
-    tokenOut: 2000, // price: 0.755
-    tokenIn: 2649.00662251656,
+    tokenIn: 2000, // price: 0.755
+    tokenOut: 2649.00662251656,
   },
   {
     address: '0xf15',
-    tokenOut: 10000, // price: 0.75
-    tokenIn: 13333.3333333333,
+    tokenIn: 10000, // price: 0.75
+    tokenOut: 13333.3333333333,
   },
   {
     address: '0xf16',
-    tokenOut: 2000, // price: 0.75
-    tokenIn: 2666.66666666667,
+    tokenIn: 2000, // price: 0.75
+    tokenOut: 2666.66666666667,
   },
   {
     address: '0xf17',
-    tokenOut: 1000, // price: 0.7
-    tokenIn: 1428.57142857143,
+    tokenIn: 1000, // price: 0.7
+    tokenOut: 1428.57142857143,
   },
   {
     address: '0xf',
-    tokenOut: 5000, // price: 0.65
-    tokenIn: 7692.30769230769,
+    tokenIn: 5000, // price: 0.65
+    tokenOut: 7692.30769230769,
   },
   {
     address: '0xf19',
-    tokenOut: 1000, // price: 0.6
-    tokenIn: 1666.66666666667,
+    tokenIn: 1000, // price: 0.6
+    tokenOut: 1666.66666666667,
   },
   {
     address: '0xf20',
-    tokenOut: 1000, // price: 0.5
-    tokenIn: 2000,
+    tokenIn: 1000, // price: 0.5
+    tokenOut: 2000,
   },
   {
     address: '0xf21',
-    tokenOut: 1000, // price: 0.5
-    tokenIn: 2000,
+    tokenIn: 1000, // price: 0.5
+    tokenOut: 2000,
   },
   {
     address: '0xf22',
-    tokenOut: 1000, // price: 0.2
-    tokenIn: 5000,
+    tokenIn: 1000, // price: 0.2
+    tokenOut: 5000,
   },
 ]
