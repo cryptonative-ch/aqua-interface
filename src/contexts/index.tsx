111bfaaac8155102b405c2cdf47b993616f6ccd8
Adam Azad <adam@adamazad.com>
2021-05-10 02:16:22 +0200

    Reduce code for UN sanction list

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 
-export const SanctionContext = React.createContext('false')
+export const SanctionContext = React.createContext<boolean>(false)
 
 export const BidModalContext = React.createContext({
   isShown: false,
