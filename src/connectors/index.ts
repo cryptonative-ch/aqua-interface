22e705b428303a39d8adfc1244fa8810f0be05ea
Adam Azad <adam@adamazad.com>
2021-05-10 02:05:17 +0200

    Add getErrorMessage handler

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,5 +1,13 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
+// Externals
+import {
+  InjectedConnector,
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from '@web3-react/injected-connector'
 import { CustomNetworkConnector } from './CustomNetworkConnector'
+import { UnsupportedChainIdError } from '@web3-react/core'
+
+// Constants
 import { ChainId, INFURA_PROJECT_ID } from 'src/constants'
 
 export const network = new CustomNetworkConnector({
@@ -13,3 +21,16 @@ export const network = new CustomNetworkConnector({
 export const injected = new InjectedConnector({
   supportedChainIds: [ChainId.RINKEBY, ChainId.XDAI],
 })
+
+export function getErrorMessage(error: Error) {
+  if (error instanceof NoEthereumProviderError) {
+    return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network."
+  } else if (error instanceof UserRejectedRequestErrorInjected) {
+    return 'Please authorize this website to access your Ethereum account.'
+  } else {
+    console.error(error)
+    return 'An unknown error occurred. Check the console for more details.'
+  }
+}
