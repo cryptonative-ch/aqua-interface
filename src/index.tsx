5246d18ae9c3c33ad0a9c94fe9b75193600aba4a
user <hamza.h.hamud@gmail.com>
2021-05-12 09:52:09 +0100

    moved dayjs to index

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,11 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { render } from 'react-dom'
 import React from 'react'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+import advanced from 'dayjs/plugin/advancedFormat'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import durationTime from 'dayjs/plugin/duration'
 
 // Redux store
 import { store } from 'src/redux/store'
@@ -18,6 +23,11 @@ import './i18n'
 
 // Extends dayjs
 dayjs.extend(DayjsRelativeTime)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(advanced)
+dayjs.extend(relativeTime)
+dayjs.extend(durationTime)
 
 function getLibrary(provider: ExternalProvider) {
   const library = new Web3Provider(provider)
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,14 +1,4 @@
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-import advanced from 'dayjs/plugin/advancedFormat'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import durationTime from 'dayjs/plugin/duration'
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(advanced)
-dayjs.extend(relativeTime)
-dayjs.extend(durationTime)
 
 export const convertUtcTimestampToLocal = (utcTimestamp: number) => {
   const utcMinutesOffset = new Date().getTimezoneOffset()
