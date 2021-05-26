97f8eba2cc6199e44b0cbadb4607009cf3c14e6b
Adam Azad <adam@adamazad.com>
2021-05-26 14:07:23 +0200

    Add useTokenContract

---
+++ src/hooks/useTokenContract.ts
@@ -0,0 +1,18 @@
+// Externals
+import { useWeb3React } from '@web3-react/core'
+import { useMemo } from 'react'
+
+// Contracts
+import { ERC20__factory } from 'src/contracts'
+
+export function useTokenContract(tokenAddress: string) {
+  const { library, account } = useWeb3React()
+
+  return useMemo(() => {
+    if (!tokenAddress || !library) {
+      return null
+    }
+
+    return ERC20__factory.connect(tokenAddress, library)
+  }, [tokenAddress, account, library])
+}
