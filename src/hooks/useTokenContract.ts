cda2fa5753891058616cc5d0add67ecd762a3bde
Adam Azad <adam@adamazad.com>
2021-05-28 12:04:16 +0200

    Use signer when available

--- src/hooks/useTokenContract.ts
+++ src/hooks/useTokenContract.ts
@@ -2,8 +2,11 @@
 import { useWeb3React } from '@web3-react/core'
 import { useMemo } from 'react'
 
-// Contracts
+// ERC20 Factory
 import { ERC20__factory } from 'src/contracts'
+
+// Utils
+import { getProviderOrSigner } from 'src/utils'
 
 export function useTokenContract(tokenAddress?: string) {
   const { library, account } = useWeb3React()
@@ -13,6 +16,6 @@ export function useTokenContract(tokenAddress?: string) {
       return null
     }
 
-    return ERC20__factory.connect(tokenAddress, library)
+    return ERC20__factory.connect(tokenAddress, getProviderOrSigner(library, account))
   }, [tokenAddress, account, library])
 }
