faa5e670d8359c3e7a4828ddfbab4e76757ba3fc
Madusha Prasanjith <mprasanjith@gmail.com>
2021-06-09 23:06:35 +0530

    Remove unused import.

--- src/views/Sale/FixedPrice.tsx
+++ src/views/Sale/FixedPrice.tsx
@@ -6,7 +6,6 @@ import { useTheme } from 'styled-components'
 import { useParams } from 'react-router-dom'
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import numeral from 'numeral'
 
 // Hooks
 import { useFixedPriceSaleQuery } from 'src/hooks/useSaleQuery'
