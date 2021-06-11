affaf8e4ce964dd470bb23da8a59e83bfde044c1
Hamza <hamza.h.hamud@gmail.com>
2021-06-11 14:10:23 +0100

    refactored

--- src/hooks/useBids.ts
+++ src/hooks/useBids.ts
@@ -9,7 +9,7 @@ import { providers } from 'ethers'
 import { useMesa } from './useMesa'
 
 // Redux actions
-import { initialBidSuccess, initialBidFailure, initialBidRequest, BidsBySaleId } from 'src/redux/bids'
+import { BidsState, initialBidSuccess, initialBidFailure, initialBidRequest, BidsBySaleId } from 'src/redux/bids'
 import { store } from 'src/redux/store'
 // Interfaces
 import { FixedPriceSalePurchase, Bid, SaleBid, FairSaleBid, SaleType, Sale } from 'src/interfaces/Sale'
@@ -21,8 +21,8 @@ import { useChain } from 'src/hooks/useChain'
 import { saleBidsQuery } from 'src/subgraph/SaleBids'
 
 interface UseBidsReturn {
-  bidsLoading: boolean
-  bidsError: Error | undefined
+  loading: boolean
+  error: Error | null
   bids: SaleBid[]
   totalBids: SaleBid[]
 }
@@ -34,13 +34,16 @@ export function useBids(
   decimal: number
 ): UseBidsReturn {
   const dispatch = useDispatch()
-  const [bidsLoading, setBidsLoading] = useState<boolean>(true)
-  const [bidsError, setBidsError] = useState<Error>()
   const { account } = useWeb3React()
   const mesa = useMesa()
-  const totalBids = useSelector(state => {
-    return state.bids.bidsBySaleId[saleId] ? state.bids.bidsBySaleId[saleId].bids : []
-  })
+  console.log(saleId)
+  const {
+    isLoading,
+    error,
+    bidsBySaleId: { [saleId]: { updatedAt, bids: totalBids } = { updatedAt: 0, bids: [] } },
+  } = useSelector(({ bids }) => bids)
+  //  const { updatedAt, bids: totalBids } = bidsBySaleId[saleId]
+
   const bids = totalBids.filter((bid: any) => bid.buyer === account)
 
   console.log(totalBids)
@@ -48,19 +51,15 @@ export function useBids(
   console.log(bids)
 
   useEffect(() => {
-    if (bids.length > 0) {
-      return setBidsLoading(false)
-    }
-    const timeNow = dayjs.utc().unix()
     // only request new bids if the delta between Date.now and saleId.updatedAt is more than 30 seconds
-    const { updatedAt } = store.getState().bids.bidsBySaleId[saleId] || timeNow
+    const chainbids = useChain(saleId, saleType, provider, decimal)
+    const timeNow = dayjs.utc().unix()
     const delta = Math.abs(updatedAt - timeNow)
-    // exit
-    // should only be called once
-    if (delta <= 3000000) {
+    if (delta <= 30 && account) {
       return
     }
 
+    //pull past bids from subgraph
     dispatch(initialBidRequest(true))
     mesa.subgraph
       .query(saleBidsQuery(saleId))
@@ -79,19 +78,14 @@ export function useBids(
         dispatch(initialBidSuccess(sales))
       })
       .catch(bidsError => {
-        setBidsError(bidsError)
         dispatch(initialBidFailure(bidsError))
       })
-      .then(() => {
-        setBidsLoading(false)
-      })
-
-    //const { bids: chainBids } = useChain(saleId, saleType, provider, decimal)
   }, [account])
+
   return {
     totalBids,
     bids,
-    bidsLoading,
-    bidsError,
+    loading: isLoading,
+    error,
   }
 }
--- src/hooks/useChain.ts
+++ src/hooks/useChain.ts
@@ -1,24 +1,17 @@
 // External
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useState } from 'react'
-import { useWeb3React } from '@web3-react/core'
+import { useDispatch } from 'react-redux'
+import react, { useEffect } from 'react'
 import { providers } from 'ethers'
 import { FixedPriceSale__factory, FairSale__factory } from 'src/contracts'
 
 // interfaces
-import { FairSaleBid, FixedPriceSalePurchase, SaleBid, SaleType } from 'src/interfaces/Sale'
+import { SaleBid, SaleType } from 'src/interfaces/Sale'
 
 // Redux
-import { fetchBidsFromChain } from 'src/redux/bids'
-import { store } from 'src/redux/store'
+import { updateBidRequest, updateBidFailure, updateBidSuccess } from 'src/redux/bids'
 import { formatDecimal } from 'src/utils/Defaults'
 
-interface UseChainReturns {
-  bids: SaleBid
-  error: Error | undefined
-}
 // TODO: REPEAT BIDS
-// TODO: CACHING
 // TODO: AGGREGATING BIDS
 // TODO: REFACTOR OF CODE
 // TODO: CLAIMTOKENS LOGIC
@@ -29,57 +22,59 @@ export function useChain(
   saleType: SaleType,
   provider: providers.JsonRpcProvider,
   decimal: number
-): UseChainReturns {
-  const [error, setError] = useState<Error>()
-  const [bids, setBids] = useState<SaleBid>()
+) {
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    return () => {
-      console.log(bids)
+    if (saleType == 'FairSale') {
+      const fairSaleContract = FairSale__factory.connect(contractAddress, provider)
+
+      fairSaleContract.on('NewOrder', async (ownerId, orderTokenOut, orderTokenIn, event) => {
+        const bids: SaleBid = {
+          id: ownerId + orderTokenIn + orderTokenOut,
+          address: ownerId,
+          tokenIn: orderTokenIn,
+          tokenOut: orderTokenOut,
+          baseSale: {
+            id: contractAddress,
+          },
+
+          createdAt: await (await provider.getBlock(event.blockNumber)).timestamp,
+          updatedAt: null,
+          deletedAt: null,
+        }
+
+        dispatch(updateBidRequest(true))
+        try {
+          dispatch(updateBidSuccess(bids))
+        } catch (error) {
+          console.log(error)
+          dispatch(updateBidFailure(error))
+        }
+      })
     }
-  }, [])
 
-  if (saleType == 'FairSale') {
-    const fairSaleContract = FairSale__factory.connect(contractAddress, provider)
+    const fixedPriceSaleContract = FixedPriceSale__factory.connect(contractAddress, provider)
 
-    fairSaleContract.on('NewOrder', async (ownerId, orderTokenOut, orderTokenIn, event) => {
-      const bids: FairSaleBid = {
-        id: ownerId + orderTokenIn + orderTokenOut,
-        address: ownerId,
-        tokenIn: orderTokenIn,
-        tokenOut: orderTokenOut,
+    fixedPriceSaleContract.on('NewPurchase', async (buyer, amount, event) => {
+      const bids: SaleBid = {
+        id: buyer + amount,
+        buyer: buyer,
+        amount: formatDecimal(amount, decimal),
         baseSale: {
           id: contractAddress,
         },
-
         createdAt: await (await provider.getBlock(event.blockNumber)).timestamp,
         updatedAt: null,
         deletedAt: null,
       }
-
-      store.dispatch(fetchBidsFromChain(bids))
+      dispatch(updateBidRequest(true))
+      try {
+        dispatch(updateBidSuccess(bids))
+      } catch (error) {
+        console.log(error)
+        dispatch(updateBidFailure(error))
+      }
     })
-  }
-
-  const fixedPriceSaleContract = FixedPriceSale__factory.connect(contractAddress, provider)
-
-  fixedPriceSaleContract.on('NewPurchase', async (buyer, amount, event) => {
-    const bids: SaleBid = {
-      id: buyer + amount,
-      buyer: buyer,
-      amount: formatDecimal(amount, decimal),
-      baseSale: {
-        id: contractAddress,
-      },
-      createdAt: await (await provider.getBlock(event.blockNumber)).timestamp,
-      updatedAt: null,
-      deletedAt: null,
-    }
-    store.dispatch(fetchBidsFromChain(bids))
-  })
-
-  return {
-    bids,
-    error,
-  }
+  }, [dispatch])
 }
