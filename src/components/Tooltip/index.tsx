408cf42aa7198c8998b455ad46c0b5a41d366e12
Madusha Prasanjith <hi@madusha.me>
2021-06-18 17:28:20 +0530

    Add generic tooltip implementation and styles.

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,61 @@
+// Externals
+import React from 'react'
+import ReactTooltip, { TooltipProps } from 'react-tooltip'
+import styled from 'styled-components'
+
+export const StyledTooltip = styled(ReactTooltip)(
+  () => ({
+    maxWidth: '30vh',
+    filter: 'drop-shadow(0px 4px 12px rgba(0, 6, 41, 0.1))',
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: 1.5,
+  }),
+  () => `
+  &.type-light {
+    background-color: #fff;
+    color: #7b7f93;
+    border: 1px solid #dddde3;
+    border-radius: 0;
+
+    &.show {
+      opacity: 1;
+    }
+
+    &.place-bottom::before {
+      border-bottom: 7px solid #dddde3;
+      top: -7px;
+    }
+
+    &.place-top::before {
+      border-top: 7px solid #dddde3;
+      bottom: -7px;
+    }
+
+    &.place-left::before {
+      border-left: 7px solid #dddde3;
+      right: -7px;
+    }
+
+    &.place-right::before {
+        border-right: 7px solid #dddde3;
+        left: -7px;
+      }
+  }
+`
+)
+
+export interface StyledTooltipProps {
+  content: React.ReactNode
+}
+
+export const Tooltip: React.FC<StyledTooltipProps & TooltipProps> = ({ children, content, ...props }) => {
+  return (
+    <>
+      <span data-tip>{children}</span>
+      <StyledTooltip multiline type="light" effect="solid" place="bottom" {...props}>
+        {content}
+      </StyledTooltip>
+    </>
+  )
+}
