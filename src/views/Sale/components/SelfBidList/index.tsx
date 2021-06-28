68c9139dd3dac416a37e11cf643c44957c2280eb
Hamza <hamza.h.hamud@gmail.com>
2021-06-28 12:02:26 +0100

    refactored component

--- src/views/Sale/components/SelfBidList/index.tsx
+++ src/views/Sale/components/SelfBidList/index.tsx
@@ -1,20 +1,10 @@
 // External
-import styled from 'styled-components'
-import { space, SpaceProps, color, ColorProps } from 'styled-system'
-import React, { useState } from 'react'
+import React from 'react'
 import numeral from 'numeral'
-
-// Components
-import { Flex } from 'src/components/Flex'
 
 // Utility
 import { isSaleOpen, isSaleClosed } from 'src/mesa/sale'
 import { useWindowSize } from 'src/hooks/useWindowSize'
-
-// Svg
-import InfoSVG from 'src/assets/svg/Info-Icon.svg'
-import MoreSVG from 'src/assets/svg/More-Icon.svg'
-import WarningSVG from 'src/assets/svg/Warning-Icon.svg'
 
 // Interfaces
 import { Sale, FairBidPick, FairSaleBid, FixedPriceSalePurchase } from 'src/interfaces/Sale'
@@ -22,83 +12,6 @@ import { formatBigInt } from 'src/utils/Defaults'
 
 // Table
 import { Table } from 'src/components/Table'
-
-type ColumnLabelProps = SpaceProps
-
-const ColumnLabel = styled.div<ColumnLabelProps>(
-  () => ({
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '12px',
-    lineHeight: '140%',
-    color: '#7B7F93',
-    marginLeft: '8px',
-  }),
-  space
-)
-
-const TokenPriceLabel = styled.div<ColorProps & SpaceProps>(
-  () => ({
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '14px',
-    lineHeight: '17px',
-    padding: '4px 8px',
-    color: '#000629',
-  }),
-  color,
-  space
-)
-
-const InfoImg = styled.img({
-  width: '14px',
-  height: '14px',
-  marginLeft: '4px',
-})
-
-type ModalContainerProps = {
-  itemIndex: number
-}
-
-const ModalContainer = styled.div<ModalContainerProps>(props => ({
-  width: '147px',
-  height: '90px',
-  padding: '8px 16px',
-  display: 'flex',
-  flexDirection: 'column',
-  position: 'absolute',
-  right: '24px',
-  top: `${props.itemIndex * 50 + 58}px`,
-  background: '#FFFFFF',
-  boxShadow: '0px 4px 12px rgba(0, 6, 41, 0.1)',
-  zIndex: 200,
-}))
-
-const ModalMenu = styled.div({
-  fontStyle: 'normal',
-  fontWeight: 500,
-  fontSize: '14px',
-  lineHeight: '21px',
-  color: '#7B7F93',
-  padding: '8px 0',
-  cursor: 'pointer',
-  ':hover': {
-    color: '#304FFE',
-  },
-})
-
-type IconImgProps = SpaceProps & {
-  isButton?: boolean
-}
-
-const IconImg = styled.img<IconImgProps>(
-  props => ({
-    width: '16px',
-    height: '16px',
-    cursor: props.isButton ? 'pointer' : 'auto',
-  }),
-  space
-)
 
 interface SelfBidListProps {
   sale: Sale
@@ -109,23 +22,8 @@ interface SelfBidListProps {
   isFixed?: boolean
 }
 
-export function SelfBidList({ sale, clearingPrice, bids, isFixed }: SelfBidListProps) {
-  const [bidMenu, setBidMenu] = useState<number>(-1)
-
+export function SelfBidList({ sale, bids, isFixed }: SelfBidListProps) {
   const { isMobile } = useWindowSize()
-
-  const toggleBidMenu = (index: number) => {
-    if (bidMenu === index) {
-      setBidMenu(-1)
-      return
-    }
-    setBidMenu(index)
-  }
-
-  const vsp = clearingPrice
-    ? formatBigInt(clearingPrice.tokenIn, sale.tokenIn.decimals) /
-      formatBigInt(clearingPrice.tokenOut, sale.tokenOut.decimals)
-    : 0
 
   const isFixedHeadData = [
     { title: 'type', flex: isMobile ? 3.5 : 3 },
@@ -151,6 +49,40 @@ export function SelfBidList({ sale, clearingPrice, bids, isFixed }: SelfBidListP
       })),
     },
   ]
+  const isFairSaleHeadData = [
+    { title: 'Token Price' },
+    { title: 'Amount' },
+    { title: 'Total Tokens', flex: isMobile ? 5 : 3 },
+    { title: 'Status', flex: isMobile ? 1 : 3 },
+  ]
+
+  const isFairSaleOpenHeadData = [
+    { title: 'Token Price' },
+    { title: 'Amount' },
+    { title: 'Est. Invested' },
+    { title: sale.tokenOut.symbol },
+  ]
+  const isFairSaleBodyData = [
+    {
+      bids: (bids as FairSaleBid[]).map(bid => ({
+        bidPrice:
+          numeral(
+            formatBigInt(bid.tokenIn, sale.tokenIn.decimals) / formatBigInt(bid.tokenOut, sale.tokenOut.decimals)
+          ).format('0.[0000]') +
+          ' ' +
+          sale.tokenIn.symbol,
+        tokenIn: numeral(formatBigInt(bid.tokenIn, sale.tokenIn.decimals)).format('0.[0000]'),
+        tokenOut: numeral(formatBigInt(bid.tokenOut, sale.tokenOut.decimals)).format('0.[0.0000]'),
+
+        totalToken:
+          numeral(
+            formatBigInt(bid.tokenIn, sale.tokenIn.decimals) * formatBigInt(bid.tokenOut, sale.tokenOut.decimals)
+          ).format('0.[0000]') +
+          ' ' +
+          sale.tokenOut.symbol,
+      })),
+    },
+  ]
 
   if (isFixed && isSaleClosed(sale)) {
     return <Table headData={isFixedHeadData} bodyData={isFixedBodyData} isClosed={true} />
@@ -160,114 +92,10 @@ export function SelfBidList({ sale, clearingPrice, bids, isFixed }: SelfBidListP
     return <Table headData={isFixedHeadDataOpen} bodyData={isFixedBodyData} />
   }
 
-  return (
-    <Flex flexDirection="column" style={{ position: 'relative' }}>
-      <Flex flexDirection="row" alignItems="center" marginBottom="8px" padding={isMobile ? '0 8px' : '0 16px'}>
-        <Flex flex={3}>
-          <ColumnLabel>{isFixed ? 'Type' : 'Token Price'}</ColumnLabel>
-        </Flex>
-        <Flex flex={3}>
-          <ColumnLabel>Amount</ColumnLabel>
-        </Flex>
-
-        {isSaleOpen(sale) ? (
-          <Flex flex={3} flexDirection="row" alignItems="center">
-            <ColumnLabel>Est. Invested</ColumnLabel>
-            <InfoImg src={InfoSVG} />
-          </Flex>
-        ) : (
-          <Flex flex={isMobile ? 5 : 3} flexDirection="row" alignItems="center">
-            <ColumnLabel>Total Tokens</ColumnLabel>
-          </Flex>
-        )}
-
-        {isSaleOpen(sale) ? (
-          <Flex flex={3} flexDirection="row" alignItems="center">
-            <ColumnLabel>{sale.tokenOut?.symbol}</ColumnLabel>
-          </Flex>
-        ) : (
-          <Flex flex={isMobile ? 1 : 3} flexDirection="row" alignItems="center" justifyContent="flex-end">
-            {!isMobile && <ColumnLabel mr="8px">Status</ColumnLabel>}
-          </Flex>
-        )}
-      </Flex>
-
-      {bids.map((bid, index: number) => {
-        const bidPrice =
-          formatBigInt(bid.tokenIn, sale.tokenIn.decimals) / formatBigInt(bid.tokenOut, sale.tokenOut.decimals)
-        const totalTokens =
-          formatBigInt(bid.tokenIn, sale.tokenIn.decimals) * formatBigInt(bid.tokenOut, sale.tokenOut.decimals)
-        return (
-          <Flex
-            key={index}
-            flexDirection="row"
-            alignItems="center"
-            minHeight="50px"
-            borderTop="1px dashed #DDDDE3"
-            padding={isMobile ? '0 8px' : '0 16px'}
-          >
-            <Flex flex={3}>
-              <TokenPriceLabel backgroundColor={vsp <= bidPrice ? '#4B9E985A' : '#E15F5F5A'}>
-                {`${numeral(bidPrice).format('0.[00]')} ${sale.tokenIn?.symbol}`}
-              </TokenPriceLabel>
-            </Flex>
-            <Flex flex={3}>
-              <TokenPriceLabel>{`${numeral(formatBigInt(bid.tokenOut, sale.tokenOut.decimals)).format('0')} ${
-                sale.tokenOut?.symbol
-              }`}</TokenPriceLabel>
-            </Flex>
-            {isSaleOpen(sale) && (
-              <Flex flex={3}>
-                <TokenPriceLabel>{`${numeral(formatBigInt(bid.tokenIn, sale.tokenIn.decimals)).format('0')} ${
-                  sale.tokenIn?.symbol
-                }`}</TokenPriceLabel>
-              </Flex>
-            )}
-            {isSaleOpen(sale) ? (
-              vsp <= bidPrice ? (
-                <Flex flex={3} flexDirection="row" alignItems="center">
-                  <TokenPriceLabel>
-                    {`${numeral(formatBigInt(bid.tokenOut, sale.tokenOut.decimals)).format('0.[00]')} ${
-                      sale.tokenOut?.symbol
-                    }`}
-                  </TokenPriceLabel>
-                  <Flex flex={1} />
-                  <IconImg src={MoreSVG} marginRight="8px" isButton={true} onClick={() => toggleBidMenu(index)} />
-                </Flex>
-              ) : (
-                <Flex flex={3} flexDirection="row" alignItems="center">
-                  <IconImg src={WarningSVG} margin="0 4px 0 8px" />
-                  <TokenPriceLabel color="#7B7F93" padding="4px 0">
-                    Below Current Price
-                  </TokenPriceLabel>
-                  <Flex flex={1} />
-                  <IconImg src={MoreSVG} marginRight="8px" isButton={true} onClick={() => toggleBidMenu(index)} />
-                </Flex>
-              )
-            ) : (
-              <Flex flex={6} flexDirection="row" alignItems="center">
-                <TokenPriceLabel color="#000629" padding="4px 0 4px 8px">
-                  {`${numeral(totalTokens).format('0.[00]')} ${sale.tokenOut.symbol}`}
-                </TokenPriceLabel>
-                <Flex flex={1} />
-                <IconImg src={WarningSVG} margin={isMobile ? '0 8px' : '0 4px 0 8px'} />
-                {!isMobile && (
-                  <TokenPriceLabel color="#000629" padding="4px 8px 4px 0">
-                    Unclaimed
-                  </TokenPriceLabel>
-                )}
-              </Flex>
-            )}
-          </Flex>
-        )
-      })}
-      {bidMenu !== -1 && (
-        <ModalContainer itemIndex={bidMenu}>
-          <ModalMenu>Withdraw Bid</ModalMenu>
-        </ModalContainer>
-      )}
-    </Flex>
-  )
+  if (isSaleOpen(sale)) {
+    return <Table headData={isFairSaleOpenHeadData} bodyData={isFairSaleBodyData as any} />
+  }
+  return <Table headData={isFairSaleHeadData} bodyData={isFairSaleBodyData as any} />
 }
 SelfBidList.defaultProps = {
   status: 'active',
