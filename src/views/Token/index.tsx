beff23c62c569fa3c8e0c30304ed9767040ff926
Hamza <hamza.h.hamud@gmail.com>
2021-07-01 19:05:28 +0100

    created reduce function

--- src/views/Token/index.tsx
+++ src/views/Token/index.tsx
@@ -5,7 +5,6 @@ import { useWeb3React } from '@web3-react/core'
 import { BigNumber } from 'ethers'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
-import _ from 'lodash'
 import { toast } from 'react-toastify'
 
 // Redux
@@ -27,6 +26,7 @@ import { ErrorMessage } from 'src/components/ErrorMessage'
 
 // claims
 import { TokenClaim } from 'src/views/Token/components/TokenClaim'
+import { useBids } from 'src/hooks/useBids'
 
 const userSalesQuery = (userAddress: string) => `
 
@@ -67,6 +67,15 @@ query getTokenClaims {
 
 `
 
+const groupBy = (collection: any, iteratee: any) => {
+  const newArray = collection.reduce((result: any, value: any, key: any) => {
+    key = iteratee(value)
+    result[value[key]] = result[value[key]] || []
+    return result
+  }, Object.create(null))
+  return newArray
+}
+
 export function TokenView() {
   const dispatch = useDispatch()
   const { account, library, chainId } = useWeb3React()
@@ -81,6 +90,7 @@ export function TokenView() {
       return
     }
 
+    console.log(filteredData)
     mesa.subgraph
       .query(userSalesQuery(account))
       .then(({ data }) => {
@@ -91,20 +101,24 @@ export function TokenView() {
             BigNumber.from(element.sale?.soldAmount) >= BigNumber.from(element.sale?.minimumRaise) &&
             unixDateNow > element.sale!.endDate
         )
+        console.log(
+          dataArray.reduce((a: any, c: any) => {
+            a[c.sale.id] = a[c.sale.id] || []
+            a[c.sale.id].push(c)
+            return a
+          }, [])
+        )
         setFilteredData(
-          _(dataArray)
-            .groupBy(purchase => purchase.sale?.id)
-            .map((purchase: any) => ({
-              sale: purchase[0].sale,
-              amount: _.reduce(
-                purchase,
-                (total, n) => {
-                  return BigNumber.from(total).add(BigNumber.from(n.amount))
-                },
-                BigNumber.from(0)
-              ),
-            }))
-            .value()
+          groupBy(dataArray, dataArray.sale.id).map((purchase: any) => ({
+            sale: purchase[0].sale,
+            amount: purchase.reduce(
+              purchase,
+              (total: any, n: any) => {
+                return BigNumber.from(total).add(BigNumber.from(n.amount))
+              },
+              BigNumber.from(0)
+            ),
+          }))
         )
       })
       .catch(error => {
