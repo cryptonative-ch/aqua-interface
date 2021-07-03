b19287587e4159ad4a732780db72b5962145bb62
Hamza <hamza.h.hamud@gmail.com>
2021-07-03 22:05:17 +0100

    testing idea

--- src/views/Token/index.tsx
+++ src/views/Token/index.tsx
@@ -27,6 +27,7 @@ import { ErrorMessage } from 'src/components/ErrorMessage'
 
 // claims
 import { TokenClaim } from 'src/views/Token/components/TokenClaim'
+import { useBids } from 'src/hooks/useBids'
 
 const userSalesQuery = (userAddress: string) => `
 
@@ -34,8 +35,6 @@ query getTokenClaims {
   fixedPriceSalePurchases(where:{
     buyer:"${userAddress}"})
   {
-    id
-    amount
     sale {
         id
       soldAmount
@@ -67,15 +66,6 @@ query getTokenClaims {
 
 `
 
-const groupBy = (collection: any, iteratee: any) => {
-  const newArray = collection.reduce((result: any, value: any, key: any) => {
-    key = iteratee(value)
-    result[value[key]] = result[value[key]] || []
-    return result
-  }, Object.create(null))
-  return newArray
-}
-
 export function TokenView() {
   const dispatch = useDispatch()
   const { account, library, chainId } = useWeb3React()
@@ -84,6 +74,7 @@ export function TokenView() {
   const [error, setError] = useState<Error>()
   const mesa = useMesa()
   const [filteredData, setFilteredData] = useState<any[] | undefined>()
+  // const (totalPurchased) = useBids(saleId, saleType)
 
   useEffect(() => {
     if (!account || !library || !chainId) {
@@ -101,23 +92,6 @@ export function TokenView() {
             unixDateNow > element.sale!.endDate
         )
         console.log(dataArray)
-        console.log(
-          dataArray
-            .reduce((a: any, c: any) => {
-              a[c.sale.id] = a[c.sale.id] || []
-              a[c.sale.id].push(c)
-              return a
-            }, [])
-            .map((purchase: any) =>
-              purchase[Object.keys(purchase)[0]].map((e: any) => ({
-                amount: e.reduce((total: any, n: any) => {
-                  return BigNumber.from(total).add(BigNumber.from(n.amount))
-                }, BigNumber.from(0)),
-
-                sale: e[0].sale,
-              }))
-            )
-        )
       })
       .catch(error => {
         setError(error)
