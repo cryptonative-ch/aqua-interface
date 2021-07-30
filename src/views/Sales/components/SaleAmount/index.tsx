71f92b29af35e9e684fb1520f2584fb9ab6f2705
Ross Neilson <rossneilson99@gmail.com>
2021-07-30 16:11:55 +0100

    Fixing amount for sale for live sale cards

--- src/views/Sales/components/SaleAmount/index.tsx
+++ src/views/Sales/components/SaleAmount/index.tsx
@@ -26,15 +26,20 @@ const SaleCardText = styled(CardText)<TextProps>`
 
 export const SaleAmount: React.FC<SaleAmountProps> = ({ sale, closed }) => {
   const isFailed = sale.soldAmount < sale.minimumRaise && closed
+  // Due to subgraph issue amount starts at 0 then is set to remaining supply
   return (
     <Flex>
       <SaleCardText isFailed={isFailed}>
         {numeral(
           sale.type == 'FairSale'
             ? formatBigInt(sale.tokensForSale, sale.tokenOut.decimals)
+            : closed
+            ? formatBigInt(sale.soldAmount) == 0
+              ? 0
+              : fixRounding(formatBigInt(sale.sellAmount) - formatBigInt(sale.soldAmount), 8)
             : formatBigInt(sale.soldAmount) == 0
-            ? 0
-            : fixRounding(formatBigInt(sale.sellAmount) - formatBigInt(sale.soldAmount), 8)
+            ? formatBigInt(sale.sellAmount)
+            : formatBigInt(sale.soldAmount)
         ).format('0,0')}
       </SaleCardText>
       <SaleCardText isFailed={isFailed} fontWeight="light">
